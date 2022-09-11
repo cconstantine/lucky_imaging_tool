060a84270e85dde4b2df9f3c7efed9aa65f09973
computing-astronomy <87629140+computing-astronomy@users.noreply.github.com>
2022-09-10 19:34:38 -0700

    Update crop.py

--- crop.py
+++ crop.py
@@ -6,25 +6,30 @@ from astropy import units as u
 from astropy.io import fits
 import pyfits
 import numpy as np
-
+from PIL import Image
 path="H:\MyWorkPythonAll"
 newpath=path+"\\"+"CroppedGoodImages"
+perH=0.7
+perW=0.7
 os.chdir(path)
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 for file in glob.glob("*.fits"):
     img=fits.open(file)
+    header=img[0].header
     img_data=img[0].data
-    width,height=img_data.shape
+    height,width=img_data.shape
     position=(width//2,height//2)
-    size=(int(width*1)+height//2,int(height*1)+height//2)
-    print(position,size,img_data)
+    #size=(int(width*perW)+height//2,int(height*perH)+height//2)
+    size=(int(height*perH),int(width*perW))
+    print("Cropping ", file)
     cutout=Cutout2D(img_data,position,size)
-    print(cutout.data)
+    #print(cutout.data)
     file2=file
+    img.close() #release the file so we can delete it
     file2=file2.split(".fits")
     file2[0]=file2[0]+"goodimg"
     file2=file2[0]+".fits"
     os.chdir(newpath)
-    fits.writeto(file2,cutout.data,overwrite=True)
+    fits.writeto(file2,cutout.data,header,overwrite=True)
     os.chdir(path)

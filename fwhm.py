c0614b59cfb779c45fa33003f11f68a32eec72e6
Chris Constantine <cconstan@gmail.com>
2022-09-12 22:37:37 -0700

    Faster search for threshold

--- fwhm.py
+++ fwhm.py
@@ -7,84 +7,25 @@ import statistics
 import glob
 import os
 import csv
+import timeit
 
-fib_n1 = None
-fib_n2 = None
-def get_next_fibinacci(n1 = None, n2 = None ):
-	global fib_n1
-	global fib_n2
 
-	# Overrule globals with arguments if set
-	if n1 != None and n2 != None:
-		fib_n1 = n1
-		fib_n2 = n2
 
-	if fib_n1 == None or fib_n2 == None:
-		fib_n1 = 0
-		fib_n2 = 1
-	else:
-		tmp = fib_n1 + fib_n2
-		fib_n1 = fib_n2
-		fib_n2 = tmp
+def binary_search(func, low, high, target, margin):
+	while True:
+		cur = (low + high) / 2.0
 
-	return fib_n1, fib_n2
-
-# Currently 100 objects detected seems to be desired for the best results.
-def determine_detection_threshold(data, background_rms_error):
-	TARGET = 100
-	objects_detected = None
-	detection_threshold = None
-
-	THRESHOLD_UPPER_LIMIT = 377 # A numebr this high does not make sense.
-
-	while detection_threshold == None or detection_threshold < THRESHOLD_UPPER_LIMIT:
-		# Placeholder for the current threshold to be tested.
-		tmp_threshold = None
-
-		# The first iteration will have no results yet.
-		if objects_detected == None:
-			'''Prime sequence to start with 3.
-			If the threshold is lower we get way to many objects
-			with a good image and this will take very long on the cpu.'''
-			tmp_threshold, _ = get_next_fibinacci(2, 3)
+		a, result = func(cur)
+		delta = target - a
+		if abs(delta) <= margin:
+			return result
+		elif delta < 0:
+			low = cur
 		else:
-			# Get next threshold in sequence.
-			tmp_threshold, _ = get_next_fibinacci()
+			high = cur
 
-		# Detect how many objects are in the image.
-		nr_objects = len(sep.extract(data, tmp_threshold, err=background_rms_error))
-		
-		# Check if we have reaced our TARGET
-		if nr_objects < TARGET:
-			# Stop iterating with increasing detection thresholds.
-			if objects_detected == None:
-				# First iteration already yields < TARGET objects.
-				# TODO: perhaps throw an error when this is true as this is likely the result of bad image.
-				objects_detected = nr_objects
-				detection_threshold = tmp_threshold
-			else:
-				# Check which threshold is closer to out target.
-				# Previous pass deviation will be >= TARGET
-				prev_deviation = objects_detected - TARGET
-
-				# current deviation will be <= TARGET
-				curr_deviation = TARGET - nr_objects
-
-				# If the current (higher threshold) yields better 
-				# results, use this. A higher threshold is faster.
-				if curr_deviation <= prev_deviation:
-					# Update values to be retured from function.
-					objects_detected = nr_objects
-					detection_threshold = tmp_threshold
-
-			# End of while loop.
-			break
-		else:
-			# Remember how many were detected with the threshold.
-			objects_detected = nr_objects
-			detection_threshold = tmp_threshold
-
-	return detection_threshold, objects_detected
+		if high - low < 0.2:
+			return None
 
 
 def fwhms(filename):
@@ -103,11 +44,13 @@ def fwhms(filename):
 	# Subtract the background from an existing array. Like data = data - bkg, but avoids making a copy of the data.
 	bkg.subfrom(data)
 
-	# Determine correct detection_threshold. (A bad image might lead to bad results, a good image is required)
-	detection_threshold, objects_detected = determine_detection_threshold(data, bkg.globalrms);
+	def search_func(threshold):
+		objects = sep.extract(data, threshold, err=bkg.globalrms)
+		return len(objects), objects
 
-	# Run object detection on the background-subtracted data.
-	objects = sep.extract(data, detection_threshold, err=bkg.globalrms)
+	objects = binary_search(search_func, 1.0, 377.0, 100, 10)
+	if objects is None:
+		return []
 
 	# Close fits image as we no longer need it.
 	f_data.close()
@@ -128,8 +71,12 @@ if __name__ == "__main__":
 	with open(os.path.join(path, "image_data.csv")) as csvfile:
 		reader = csv.DictReader(csvfile)
 		for row in reader:
-			# print(row)
+			# # print(row)
+			# def timed():
+			# 	fwhms(os.path.join(path, row['filename']))
+			# print(timeit.Timer(timed).timeit(number=1))
 
 			results = fwhms(os.path.join(path, row['filename']))
+			# print(timeit.Timer())
 			result = statistics.mean(results)+statistics.stdev(results) if len(results) > 0 else 0.0
 			print("{:2.5f},{:2.5f},{}".format(float(row['fwhm_pixels']), result, row['filename']))

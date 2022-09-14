f294398d512bdc907e81a66594f760f0c15cfdee
thomasvankleef <lekkerbekje>
2022-09-14 18:11:09 +0200

    Ensure proper shutdown of script from ctrl-c

--- lucky_imaging_tool.py
+++ lucky_imaging_tool.py
@@ -10,14 +10,28 @@ from datetime import datetime, timedelta
 from crop import crop
 import common
 from multiprocessing import Pool, cpu_count
+import tqdm
+import signal
+
+exit_program = False
+def handler(signum, frame):
+    global exit_program
+
+    if exit_program == False:
+        exit_program = True
+
+signal.signal(signal.SIGINT, handler)
 
 def main():
+    global exit_program
+
     data = common.init()
-    
+    monitoring_path = os.path.abspath(data["path"])
     with Pool(processes=cpu_count()) as pool:
-        print("Monitoring ", os.path.abspath(data["path"])) #Just an indicator that monitoring is active
-        while(True):#monitors if NINA is open
-            print("Monitoring filepath...") #Just an indicator that monitoring is active
+        print("Monitoring ", monitoring_path) #Just an indicator that monitoring is active
+
+        while(exit_program == False):#monitors if NINA is open
+            print("Monitoring filepath {}".format(monitoring_path)) #Just an indicator that monitoring is active
             for file in common.get_fits_from_folder(data["path"]):
                 pool.apply_async(common.handle_file,
                                 (file, data["cropped_folder"],
@@ -28,6 +42,11 @@ def main():
                                  data["perH"]))
 
             time.sleep(1)
+
+        print("Stopping processes")
+        # Wait for pool to finish
+        pool.close()
+        pool.join()
 
 if __name__ == "__main__":
     main()

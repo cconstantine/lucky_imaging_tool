Integrated correct fwhm calculation

common.py
@@ -8,6 +8,7 @@ import json
 from astropy.io import fits
 import traceback
 import gc
+import numpy as np
 
 CONFIG_FILE="lucky_imaging.cnf.json"
 def save_config_to_file(data):
@@ -46,7 +47,7 @@ def parse_args():
     perW=float(input("Fraction of the width to keep (0 - 1, where 0 crops all, 1 keeps full image), ex: 70%=0.7: "))
     perH=float(input("Fraction of the height to keep (0 - 1, where 0 crops all, 1 keeps full image),, ex: 70%=0.7: "))
     path=input("Enter Filepath for monitoring images (e.g. C:\Astrophotography\M31 ): ")
-    print("Next select the FWHM or half-flux radius over which images will be deleted")
+    print("Next select the FWHM in arcsecond threshold over which images will be deleted. Any image with a value larger then X (Exmaple: 3.0) will be deleted.")
     print("Note that this is not the FWHM in arc seconds, but in pixels")
     FWHMthresh=float(input("Rejection threshold (FWHM in arcseconds, or HFR in arcseconds) above which to delete files: "))
     del_uncrop=str(input("Delete original uncropped images (Y/N):"))
@@ -96,18 +97,18 @@ def set_process_priority():
         PID.nice(psutil.BELOW_NORMAL_PRIORITY_CLASS)
 
  
-def calculate_fwhm(data, FWHMthresh, numStar, focal_length, pizel_size):
+def calculate_fwhm(data, FWHMthresh, numStar, focal_length, pixel_size):
     # Use calculator to get fwhm from image.
-    fwhm = Calculator(numStar).fwhm(data, focal_length, pizel_size)
-
+    objects, fwhm_px, fwhm_arcsec = Calculator(numStar, FWHMthresh).fwhm(focal_length, pixel_size, data)
+    
     # If the fwhm is above the threshold it shall be removed.
-    is_fwhm_above_threshold = fwhm > FWHMthresh
+    is_fwhm_above_threshold = fwhm_px > FWHMthresh
     if is_fwhm_above_threshold:
-        print("fwhm {:2.5f} > threshold {:2.5f}".format(fwhm, FWHMthresh))
+        print("fwhm {:2.5f} > threshold {:2.5f}".format(fwhm_px, FWHMthresh))
     else:
-        print("fwhm {:2.5f} <= threshold {:2.5f}".format(fwhm, FWHMthresh))
+        print("fwhm {:2.5f} <= threshold {:2.5f}".format(fwhm_px, FWHMthresh))
 
-    return fwhm, is_fwhm_above_threshold
+    return fwhm_px, fwhm_arcsec, is_fwhm_above_threshold
 
 def crop_file(file, fits_data, fits_header, perW, perH, destination_folder):
     # Where to store the cropped file.
@@ -148,16 +149,32 @@ def handle_file(original, cropped_folder, moved_orignals_folder, del_uncrop, FWH
             is_fits_file = is_file_a_fits_file(f_fits[0].header)
 
             if is_fits_file:
+                data = f_fits[0].data
+
+                # TODO: add all other bayer patterns
+                try:
+                    bayer_pattern = header["BAYERPAT"]
+                    print("bayer_pattern is {}".format(bayer_pattern))
+
+                    debayered_image = cv2.cvtColor(data, cv2.COLOR_BayerRG2BGR)
+                    data = cv2.normalize(debayered_image, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_16U)
+
+                except Exception as e:
+                    bayer_pattern = None
+
+                # Convert to 32 bit integer
+                data=np.array(data,dtype='int32')
+
                 #Calculate fwhm
-                fwhm, is_fwhm_above_threshold = calculate_fwhm(f_fits[0].data, FWHMthresh, numStar, fl, pixelSize)
+                fwhm_px, fwhm_arcsec, is_fwhm_above_threshold = calculate_fwhm(data, FWHMthresh, numStar, fl, pixelSize)
 
                 # Crop image is cropping parameters are set (unset is equal to original) and fwhm is OK
                 if(do_crop and not is_fwhm_above_threshold):
                     crop_file(original, f_fits[0].data, f_fits[0].header, perW, perH, cropped_folder)
         return True, original, is_fwhm_above_threshold, do_crop, del_uncrop, moved_orignals_folder, is_fits_file
     except Exception as e:
-        traceback.print_exc()
-        pass
+        # traceback.print_exc()
+        return False, original, False, False, 'n', moved_orignals_folder, is_fits_file
 
 
 def test_args():

fwhm.py
@@ -11,85 +11,106 @@ import timeit
 
 
 class Calculator:
-	def __init__(self,star=100,thr=10):
-		self.low = 0.0
-		self.high = 500.0
-		self.star=star
-		self.thr=thr
+    def __init__(self, star=100, thr=10):
+        self.low = 0.0
+        self.high = 500.0
+        self.star=star
+        self.thr=thr
 
 
-	def binary_search(self, func, target, margin):
-		low = self.low
-		high = self.high
+    def binary_search(self, func, data, target, margin, background, kernel):
+        low = self.low
+        high = self.high
 
-		while True:
-			cur = (low + high) / 2.0
+        while True:
+            cur = (low + high) / 2.0
 
-			a, result = func(cur)
-			delta = target - a
-			if abs(delta) <= margin:
-				return result
-			elif delta < 0:
-				low = cur
-			else:
-				high = cur
+            a, result = func(data, cur, background, kernel)
+            delta = target - a
+            if abs(delta) <= margin:
+                return result
+            elif delta < 0:
+                low = cur
+            else:
+                high = cur
 
-			if high - low < 0.2:
-				return None
+            if high - low < 0.2:
+                return None
 
+    def search_func(self, data, threshold, bkg, kernel):
+        objects = sep.extract(data, threshold, err=bkg.globalrms, filter_type="matched", filter_kernel=kernel)
+        return len(objects), objects
 
-	def fwhms(self, fitsdata):
-		# Convert to 32 bit integer
-		data=np.array(fitsdata,dtype='int32')
+    def fwhm(self, focal_length, pixel_size, data):
+        # Representation of spatially variable image background and noise.
+        bkg = sep.Background(data)
 
-		# Representation of spatially variable image background and noise.
-		bkg = sep.Background(data)
+        # evaluate background as 2-d array, same size as original image
+        bkg_image = bkg.back()
 
-		# Subtract the background from an existing array. Like data = data - bkg, but avoids making a copy of the data.
-		bkg.subfrom(data)
+        # Get backround noise
+        bkg_rms = bkg.rms()
 
-		def search_func(threshold):
-			objects = sep.extract(data, threshold, err=bkg.globalrms)
-			return len(objects), objects
+        # Subtract the background from an existing array. Like data = data - bkg, but avoids making a copy of the data.
+        bkg.subfrom(data)
 
-		objects = self.binary_search(search_func, self.star, self.thr)
-		if objects is None:
-			return []
+        # Create a filter-kernal with the shape of a star (round point ;))
+        kernel = np.array([[1., 2., 3., 2., 1.],
+        [2., 3., 5., 3., 2.],
+        [3., 5., 8., 5., 3.],
+        [2., 3., 5., 3., 2.],
+        [1., 2., 3., 2., 1.]])
 
-		#### Determine FWHMs ####
-		results=[]
+        objects = self.binary_search(self.search_func, data, self.star, self.thr, bkg, kernel)
+        if objects is None:
+            return None
 
-		for i in range(len(objects)):
-			thing=(float(objects['a'][i])**2) + (float(objects['b'][i])**2)
-			results.append(2 * (sqrt((np.log(2)) * thing)))
+        # Required in calculating the radius of the circles
+        kronrad, krflag = sep.kron_radius(data, objects["x"], objects["y"], objects["a"], objects["b"], objects["theta"], 6.0)
 
-		return results
+        # The following section removes any stars which are too small.
+        # It will remove from objects if they are smaller then 1.75 px in diameter.
+        r_min = 1.75  # minimum diameter = 3.5
+        use_circle = kronrad * np.sqrt(objects["a"], objects["b"]) < r_min
 
-	def fwhm(self, fitsdata, focal_length, pixel_size):
-		f = self.fwhms(fitsdata)
-		if len(f) > 0:
-			fwhmPix=0.6188*(statistics.mean(f)+statistics.stdev(f))+(0.5301)
-			return fwhmPix*((pixel_size/focal_length)*206.3)
-		else:
-			return 0.0
+        # Calculate the FWHMs of the stars:
+        fwhm = 2.0 * (np.log(2) * (objects['a'] ** 2.0 + objects['b'] ** 2.0)) ** 0.5
 
+        # Remove any larger than.
+        fwhm = fwhm[fwhm < 30]
 
+        # print(fwhm)
+        # exit()
+        arcsec_constant = 206.265
+        pixel_scale = ((pixel_size / focal_length) * arcsec_constant)
+        perfect_arcsec_per_px = 122 / focal_length
+        image_scale = arcsec_constant * pixel_size / focal_length
+
+        # Print a comparable string as test-my-scope
+        mean, stdev = np.mean(fwhm), np.std(fwhm)
+        final_fwhm = mean + stdev
+        final_arcsec = (mean + stdev) * image_scale
+        print("FWHM: {} px / {} arcsec".format(final_fwhm, final_arcsec))
+        # exit()
+        return objects, final_fwhm, final_arcsec
+
+# Broken fix fwhm params
 if __name__ == "__main__":
-	calc = Calculator()
-	path = os.path.join("test", "images")
-	print("DSS,FWHM,filename")
-	with open(os.path.join(path, "image_data.csv")) as csvfile:
-		reader = csv.DictReader(csvfile)
-		for row in reader:
-			# # print(row)
-			# def timed():
-			# 	fwhms(os.path.join(path, row['filename']))
-			# print(timeit.Timer(timed).timeit(number=1))
+    calc = Calculator()
+    path = os.path.join("test", "images")
+    print("DSS,FWHM,filename")
+    with open(os.path.join(path, "image_data.csv")) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # # print(row)
+            # def timed():
+            #   fwhms(os.path.join(path, row['filename']))
+            # print(timeit.Timer(timed).timeit(number=1))
 
-			# Open a file to use
-			with fits.open(row['filename']) as fitsdata:
-				result = calc.fwhm(os.path.join(path, fitsdata))
-				# print(timeit.Timer())
-				# temp=statistics.mean(results)+statistics.stdev(results)
-				# result = (0.6188*temp) + 0.5301 if len(results) > 0 else 0.0
-				print("{:2.5f},{:2.5f},{}".format(float(row['fwhm_pixels']), result, row['filename']))
+            # Open a file to use
+            with fits.open(row['filename']) as fitsdata:
+                result = calc.fwhm(os.path.join(path, fitsdata))
+                # print(timeit.Timer())
+                # temp=statistics.mean(results)+statistics.stdev(results)
+                # result = (0.6188*temp) + 0.5301 if len(results) > 0 else 0.0
+                print("{:2.5f},{:2.5f},{}".format(float(row['fwhm_pixels']), result, row['filename']))